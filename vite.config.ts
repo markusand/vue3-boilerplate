9709dab72d53167c89175894e0ae6d06a35ef595
Marc Vilella <mvilella.and@gmail.com>
2024-04-15 12:52:00 +0200

    feat: Replace yaml plugin
    This plugin integrates better with TypeScript

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
-import yaml from '@rollup/plugin-yaml';
+import yaml from '@modyfi/vite-plugin-yaml';
 
 // https://vitejs.dev/config/
 export default defineConfig({
